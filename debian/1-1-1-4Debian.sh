36707891b800667bc3e6fe35a3ae91b9adce701d
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-08-29 21:34:55 +0800

    Add files via upload

---
+++ debian/1-1-1-4Debian.sh
@@ -0,0 +1,20 @@
+
+#!/bin/sh
+section=1
+auditno="1.1.1.4"
+auditname="Ensure mounting of hfsplus filesystem is diabled"
+
+cmd=$(/usr/sbin/modprobe -n -v hfsplus | grep -E '(hfsplus|install)')
+cmd1=$(lsmod | grep hfsplus)
+correct="install /bin/true "
+
+
+if [ "$cmd" = "$correct" ] && [ -z "$cmd1" ]
+then
+        auditres="\033[32mPass\033[m"
+
+else
+        auditres="\033[31mFail\033[m"
+
+fi
+echo "${auditno} \t${auditname} \t\t\t[${auditres}]"
