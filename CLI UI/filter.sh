9299785e4f1b9fb23ceb164033af4e3231410058
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-08-30 22:19:47 +0800

    Create filter.sh

---
+++ CLI UI/filter.sh
@@ -0,0 +1,45 @@
+
+
+echo  "Hello, which part of the report would u like to search? \n Use -h to see search filter which have been preset "
+read option
+help="-h"
+file="/script/report.txt"
+
+#echo "User wants to search for:$option"
+
+#findings=$(grep $option /script/report.txt)
+#echo "$findings"
+
+if [ -f "$file" ]
+then
+
+if [ "$option" = 1 ]
+then
+echo "User wants to search for: Pass"
+grep Pass /script/report.txt
+
+elif [ "$option" = 2 ]
+then
+echo "User wants to search for: Fail"
+grep Fail /script/report.txt
+
+elif [ "$option" = 3 ]
+then
+echo "User wants to search for: Section 1.1"
+grep 1.1 /script/report.txt
+
+elif [ "$option" = "$help" ]
+then
+cat /script/help_filter.txt
+
+else
+
+findings=$(grep $option /script/report.txt)
+echo "$findings"
+
+fi
+
+else
+echo "Please run option 1: run Linux benchmarking before doing seach. Currently file does not exist" 
+
+fi
