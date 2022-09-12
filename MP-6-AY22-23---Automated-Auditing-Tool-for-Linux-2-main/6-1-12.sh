172a436c819abe037eaf018b16cada72be37a083
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 16:07:34 +0800

    Update 6-1-12.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/6-1-12.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/6-1-12.sh
@@ -4,7 +4,7 @@ section=6
 auditNo="6.1.12"
 auditName="Ensure no ungrouped files or directories exist"
 
-cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -nogroup | grep 2> \dev\null)
+cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -nogroup 2> \dev\null)
 auditres="\033[31mFail\033[m"
 if  [ "$cmd" = null ] 
 then
