aa64a2bf533ba2f8585b928a7e1b6f0edd4c755a
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 15:50:08 +0800

    Update 4-1-7.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-7.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-7.sh
@@ -4,8 +4,8 @@ section=4
 auditNo="4.1.7"
 auditName="Ensure login and logout events are collected"
 
-cmd1=$(grep logins /etc/audit/rules.d/*.rules)
-cmd2=$(auditctl -l | grep logins)
+cmd1=$(grep logins /etc/audit/rules.d/*.rules 2> \dev\null)
+cmd2=$(auditctl -l 2> \dev\null | grep logins)
 auditres="\033[31mFail\033[m"
 correct1="-w /var/log/faillog -p wa -k logins -w /var/log/lastlog -p wa -k logins -w /var/log/tallylog -p wa -k logins"
 correct2="-w /var/log/faillog -p wa -k logins -w /var/log/lastlog -p wa -k logins -w /var/log/tallylog -p wa -k logins"
