5198c25bc29a0244efd93721b9cc298bad622a41
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 20:24:49 +0800

    Update 4-1-8.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-8.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-8.sh
@@ -4,7 +4,7 @@ section=4
 auditNo="4.1.8"
 auditName="Ensure session initiation information is collected"
 
-cmd1=$(grep -E '(session|logins)' /etc/audit/rules.d/*.rules)
+cmd1=$(grep -E '(session|logins)' /etc/audit/rules.d/*.rules 2> \dev\null )
 cmd2=$(auditctl -l 2> \dev\null | grep -E '(session|logins)')
 auditres="\033[31mFail\033[m"
 correct1="-w /var/run/utmp -p wa -k session -w /var/log/wtmp -p wa -k logins -w /var/log/btmp -p wa -k logins"
@@ -14,4 +14,4 @@ then
 auditres="\033[32mPass\033[m"
 fi
 
-echo "${auditNo} \t\t${auditName} \t[${auditres}]"
+echo "${auditNo} \t\t${auditName} \t\t\t\t\t\t[${auditres}]"
