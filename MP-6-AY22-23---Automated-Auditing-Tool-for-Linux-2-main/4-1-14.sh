067963501f6661d04b4104d93c6638d7c18ad688
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 20:36:41 +0800

    Update 4-1-14.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-14.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-14.sh
@@ -4,8 +4,8 @@ section=4
 auditNo="4.1.14"
 auditName="Ensure changes to system administration scope (sudoers) is collected"
 
-cmd1=$(grep scope /etc/audit/rules.d/*.rules)
-cmd2=$(auditctl -l | grep scope)
+cmd1=$(grep scope /etc/audit/rules.d/*.rules 2> \dev\null)
+cmd2=$(auditctl -l 2> \dev\null | grep scope)
 auditres="\033[31mFail\033[m"
 correct1="-w /etc/sudoers -p wa -k scope -w /etc/sudoers.d/ -p wa -k scope"
 correct2="-w /etc/sudoers -p wa -k scope -w /etc/sudoers.d/ -p wa -k scope"
