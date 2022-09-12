1388f34a46837b10252218719e91c1c08418e8ef
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 20:32:07 +0800

    Update 4-1-12.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-12.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-12.sh
@@ -4,8 +4,8 @@ section=4
 auditNo="4.1.12"
 auditName="Ensure successful file system mounts are collected"
 
-cmd1=$(grep mounts /etc/audit/rules.d/*.rules)
-cmd2=$(auditctl -l | grep mounts)
+cmd1=$(grep mounts /etc/audit/rules.d/*.rules 2> \dev\null)
+cmd2=$(auditctl -l 2> \dev\null | grep mounts)
 auditres="\033[31mFail\033[m"
 correct1="-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts"
 correct2="a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=-1 -F key=mounts"
