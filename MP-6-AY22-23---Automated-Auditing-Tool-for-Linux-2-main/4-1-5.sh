7123f34ebc3af01405bd059f106efab71d8bfe6a
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 20:05:34 +0800

    Update 4-1-5.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-5.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-5.sh
@@ -4,8 +4,8 @@ section=4
 auditNo="4.1.5"
 auditName="Ensure events that modify the system's network environment are collected"
 
-cmd1=$(grep system-locale /etc/audit/rules.d/*.rules)
-cmd2=$(auditctl -l | grep system-locale)
+cmd1=$(grep system-locale /etc/audit/rules.d/*.rules 2> \dev\null)
+cmd2=$(auditctl -l 2> \dev\null | grep system-locale 2> \dev\null)
 auditres="\033[31mFail\033[m"
 correct1="-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale -w /etc/issue -p wa -k system-locale -w /etc/issue.net -p wa -k system-locale -w /etc/hosts -p wa -k system-locale -w /etc/network -p wa -k system-locale"
 correct2="-a always,exit -F arch=b32 -S sethostname,setdomainname -F key=system-locale -w /etc/issue -p wa -k system-locale -w /etc/issue.net -p wa -k system-locale -w /etc/hosts -p wa -k system-locale -w /etc/network -p wa -k system-locale"
