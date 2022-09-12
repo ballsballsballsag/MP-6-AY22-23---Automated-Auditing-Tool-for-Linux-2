b12632409648188775d396be7130cc66388a4563
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 20:02:11 +0800

    Update 4-1-4.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-4.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-4.sh
@@ -4,8 +4,8 @@ section=4
 auditNo="4.1.4"
 auditName="Ensure events that modify user/group information are collected"
 
-cmd1=$(grep identity /etc/audit/rules.d/*.rules)
-cmd2=$(auditctl -l | grep identity)
+cmd1=$(grep identity /etc/audit/rules.d/*.rules 2> \dev\null)
+cmd2=$(auditctl -l 2> \dev\null | grep identity 2> \dev\null)
 auditres="\033[31mFail\033[m"
 correct1="-w /etc/group -p wa -k identity -w /etc/passwd -p wa -k identity -w /etc/gshadow -p wa -k identity -w /etc/shadow -p wa -k identity -w /etc/security/opasswd -p wa -k identity"
 correct2="-w /etc/group -p wa -k identity -w /etc/passwd -p wa -k identity -w /etc/gshadow -p wa -k identity -w /etc/shadow -p wa -k identity -w /etc/security/opasswd -p wa -k identity"
