d9eeb079c72c4ef54d274dd233878cd804bbb55e
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 20:38:07 +0800

    Update 4-1-16.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-16.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-16.sh
@@ -4,8 +4,8 @@ section=4
 auditNo="4.1.16"
 auditName="Ensure kernel module loading and unloading is collected"
 
-cmd1=$(grep modules /etc/audit/rules.d/*.rules)
-cmd2=$(auditctl -l | grep modules)
+cmd1=$(grep modules /etc/audit/rules.d/*.rules 2> \dev\null)
+cmd2=$(auditctl -l 2> \dev\null | grep modules)
 auditres="\033[31mFail\033[m"
 correct1="-w /sbin/insmod -p x -k modules -w /sbin/rmmod -p x -k modules -w /sbin/modprobe -p x -k modules -a always,exit -F arch=b32 -S init_module -S delete_module -k modules"
 correct2="-w /sbin/insmod -p x -k modules -w /sbin/rmmod -p x -k modules -w /sbin/modprobe -p x -k modules -a always,exit -F arch=b32 -S init_module,delete_module -F key=modules"
