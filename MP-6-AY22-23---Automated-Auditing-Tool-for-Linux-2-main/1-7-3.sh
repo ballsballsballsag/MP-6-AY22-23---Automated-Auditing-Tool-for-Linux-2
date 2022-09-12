c5c5172fc878eec4dcc0634e3a248008f609481e
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-09-12 20:48:04 +0800

    Update 1-7-3.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/1-7-3.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/1-7-3.sh
@@ -3,7 +3,7 @@ section=1
 auditno="1.7.3"
 auditname="Ensure remote login warning banner is configured properly"
 
-cmd=$(grep -Eis "(\\\v|\\\r|\\\m|\\\s|$(grep '^ID=' /etc/os-release | cut -d= -f2 | sed -e 's/"//g'))" /etc/issue.net)
+cmd=$(grep -E -i "(\\\v|\\\r|\\\m|\\\s|$(grep '^ID=' /etc/os-release | cut -d= -f2 | sed -e 's/"//g'))" /etc/issue.net 2>/dev/null)
 #cmd1=$(grep -E '^\s*chmod\s+[0-7][0-7][0-7]\s+\$\{grub_cfg\}\.new' -A 1 -B1 /usr/sbin/grub-mkconfig | grep password)
 #| grep chmod`
 #| cut -b 9-11`
