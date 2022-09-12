2da9c8521fbc6d5f8cb6e551f44f006dad8c0f84
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 20:38:37 +0800

    Update 4-1-17.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-17.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-17.sh
@@ -4,7 +4,7 @@ section=4
 auditNo="4.1.17"
 auditName="Ensure the audit configuration is immutable"
 
-cmd1=$(grep modules /etc/audit/rules.d/*.rules)
+cmd1=$(grep modules /etc/audit/rules.d/*.rules 2> \dev\null)
 auditres="\033[31mFail\033[m"
 correct1="-e 2"
 if  [ "$cmd1" = "$correct1" ] 
