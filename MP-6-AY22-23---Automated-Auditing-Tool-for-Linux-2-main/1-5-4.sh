ca91ba44943804170e828088bf50161527fbf95e
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-09-12 20:41:47 +0800

    Update 1-5-4.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/1-5-4.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/1-5-4.sh
@@ -3,31 +3,24 @@ section=1
 auditno="1.5.4"
 auditname="Ensure core dumps are restricted"
 
-cmd=$(grep -Es '^(\*|\s).*hard.*core.*(\s+#.*)?$' /etc/security/limits.conf /etc/security/limits.d/* | cut -b 27-39)
-cmd1=$(sysctl fs.suid_dumpable)
-cmd2=$(grep "fs.suid_dumpable" /etc/sysctl.conf /etc/sysctl.d/* > coredumps.txt)
-cmd3=$(systemctl is-enabled coredump.service)
-cmd4=$(cat /etc/systemd/coredump.conf | grep Storage)
-cmd5=$(cat /etc/systemd/coredump.conf | grep ProcessSizeMax)
-first=$(cat coredumps.txt | head -n 1 | cut -b 18-37)
-second=$(cat coredumps.txt | tail -n 1 | cut -b 30-49)
+cmd1=$(grep -Es '^(\*|\s).*hard.*core.*(\s+#.*)?$' /etc/security/limits.conf /etc/security/limits.d/* 2> /dev/null)
+cmd2=$(sysctl fs.suid_dumpable 2> /dev/null)
+cmd3=$(grep "fs.suid_dumpable" /etc/sysctl.conf /etc/sysctl.d/* 2>/dev/null)
+
 correct="* hard core 0"
+
+
 correct1="fs.suid_dumpable = 0"
-correct2="Storage=none"
-correct3="ProcessSizeMax=0"
 
 
 
-if [ ! -z "cmd3" ] && [ "cmd4" = "correct2" ] && [ "$cmd5" = "correct3" ] && [ "$cmd" = "$correct" ] && [ "$first" = "$correct1" ] && [ "$second" = "$correct1" ]
 
-# && [ "$cmd1" = "$correct1" ]
-# && [ "$first" = "$correct1" ]&& [ "$second" = "$correct1" ]
 
+if [ "$cmd1" = "$correct" ] && [ "cmd2" = "correct1" ] && [ "$cmd3" = "correct1" ] 
 then
-	auditres="\033[32mPass\033[m"
-
+        auditres="\033[32mPass\033[m"
 else
-	auditres="\033[31mFail\033[m"
+        auditres="\033[31mFail\033[m"
 
 
 fi
