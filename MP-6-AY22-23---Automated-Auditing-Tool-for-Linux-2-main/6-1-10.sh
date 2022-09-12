308cf97905d9bba458c7ce914345508187d17094
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 16:22:11 +0800

    Update 6-1-10.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/6-1-10.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/6-1-10.sh
@@ -4,7 +4,7 @@ section=6
 auditNo="6.1.10"
 auditName="Ensure no world writable files exist"
 
-cmd= $(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -0002 | grep 2> \dev\null)
+cmd= $(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -0002 | 2> \dev\null)
 auditres="\033[31mFail\033[m"
 if  [ "$cmd" = null ] 
 then
