eacd4075c5640c3817d1ee5541d203e19bbc6c73
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-12 20:37:26 +0800

    Update 4-1-15.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-15.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-1-15.sh
@@ -4,8 +4,8 @@ section=4
 auditNo="4.1.15"
 auditName="Ensure system administrator command executions (sudo) are collected"
 
-cmd1=$(grep actions /etc/audit/rules.d/*.rules)
-cmd2=$(auditctl -l | grep actions)
+cmd1=$(grep actions /etc/audit/rules.d/*.rules 2> \dev\null)
+cmd2=$(auditctl -l 2> \dev\null | grep actions)
 auditres="\033[31mFail\033[m"
 correct1="/etc/audit/rules.d/cis.rules:-a exit,always -F arch=b32 -C euid!=uid -F euid=0 -Fauid>=1000 -F auid!=4294967295 -S execve -k actions"
 correct2="-a always,exit -F arch=b32 -S execve -C uid!=euid -F euid=0 -F auid>=1000 -F auid!=-1 -F key=actions"
