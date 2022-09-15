1b3fb1dfe9cb965165133bd42bf78a35272c3772
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-09-15 20:38:12 +0800

    Update jobs.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/jobs.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/jobs.sh
@@ -1,6 +1,6 @@
 
 
-echo  "Hello, at which intervals of the year would you like the benchmark to run automatically?\nUse -h for the help option\nEnsure that the command crontab -u <user> -e has been ran before running this service."
+echo  "Hello, at which intervals of the year would you like the benchmark to run automatically?\nUse -h for the help option. It will show the dates wherby the automating auditing will be executed on.\nE>
 read option
 help="-h"
 
