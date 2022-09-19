e4cbf40e4dd6ffee65f7e2c232ac8e815182bb9a
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-19 15:42:21 +0800

    Update 4-4.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-4.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/4-4.sh
@@ -6,7 +6,7 @@ auditName="Ensure logrotate assigns appropriate permissions"
 
 cmd=$(grep -Es "^\s*create\s+\S+" /etc/logrotate.conf /etc/logrotate.d/* | grep -E -v "\s(0)?[0-6][04]0\s" | grep 2> \dev\null)
 auditres="\033[31mFail\033[m"
-if  [ "$cmd" == null ]
+if  [ "$cmd" = null ]
 then
 auditres="\033[32mPass\033[m"
 fi
