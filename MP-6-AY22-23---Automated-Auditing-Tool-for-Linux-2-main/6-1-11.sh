ede89a7d9134da269b789fbe2e9e1d740bd600cf
nigeldreams <68185149+nigeldreams@users.noreply.github.com>
2022-09-19 16:07:21 +0800

    Update 6-1-11.sh

--- MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/6-1-11.sh
+++ MP-6-AY22-23---Automated-Auditing-Tool-for-Linux-2-main/6-1-11.sh
@@ -4,7 +4,7 @@ section=6
 auditNo="6.1.11"
 auditName="Ensure no unowned files or directories exist"
 
-cmd=$(sudo df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -nouser | grep 2> \dev\null)
+cmd=$(sudo df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -nouser 2> \dev\null)
 auditres="\033[32mPass\033[m"
 if  [ "$cmd" != null ] 
 then
